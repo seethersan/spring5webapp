d2f0976a78d46036fbbb12da537fb01a51cc9626
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2019-12-27 08:57:18 -0500

    [feature] added Author, Book and Publisher Controllers with their templates

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/PublisherController.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PublisherController {
+    private PublisherRepository publisherRepository;
+
+    public PublisherController(PublisherRepository publisherRepository){
+        this.publisherRepository = publisherRepository;
+    }
+
+    @RequestMapping("/publishers")
+    public String getPublisher(Model model){
+        model.addAttribute("publishers", publisherRepository.findAll());
+        return "publishers";
+    }
+}
